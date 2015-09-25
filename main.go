6820a24b6003af690f1f49fdc46dc3e922dc4fec
mukulrawat1986 <mukulsrawat@gmail.com>
2015-09-25 22:36:34 +0530

    made corrections to type and removed errors

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ type Response struct {
 
 // Add a string method to our Item type so that it implements the Stringer interface
 // and makes it easier for us to print
-func (i item)String() string{
+func (i Item)String() string{
     return fmt.Sprintf("%s\n%s", i.Title, i.URL)
 }
 
